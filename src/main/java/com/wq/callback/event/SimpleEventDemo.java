a2fac1a3ac9fc3aa77079a09d285cdbec7eba23c
MichaelPaul0416 <18357903752m0@sina.cn>
2017-10-05 22:03:35 +0800

    [20171005]观察者模式

---
+++ src/main/java/com/wq/callback/event/SimpleEventDemo.java
@@ -0,0 +1,51 @@
+package com.wq.callback.event;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author:wangqiang20995
+ * @description:简单模拟事件驱动
+ * @Date:2017/10/3
+ */
+public class SimpleEventDemo {
+    public static void main(String args[]){
+        Event event = new Event();
+        CallBack callBack = new OnClick();
+        event.registry(callBack);
+
+        event.fireRegistrySucceed();
+    }
+
+}
+
+
+class Event{
+    private List<CallBack> queue;
+
+    public Event(){
+        queue = new ArrayList<>();
+    }
+
+    public boolean registry(CallBack callBack){
+        return queue.add(callBack);
+    }
+
+    public void fireRegistrySucceed(){
+        for(CallBack callBack : queue) {
+            callBack.run();
+        }
+    }
+}
+
+class OnClick implements CallBack{
+
+    @Override
+    public void run() {
+        System.out.println("You click me just now");
+    }
+}
+
+interface CallBack{
+    void run();
+}
